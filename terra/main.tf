af0d46d83469312d644c35826f2e89090d008955
Kadir Gökhan Sezer <kadirgokhansezer@Kadirs-MacBook-Air-3.local>
2023-10-14 19:20:50 +0300

    hm2

--- terra/main.tf
+++ terra/main.tf
@@ -1,36 +1,228 @@
+/* 
+aws ec2 run-instances --image-id ami-053b0d53c279acc90 --instance-type t2.micro --subnet-id 0 --security-group-ids 657476548658 --key-name "boun.pem" --list profile data_engineer
+aws ec2 create-network-interface --subnet-id subnet-0e32e5e646620069b --profile network_profile
+aws s3 list --profile XXXX
+*/
+resource "aws_iam_role_policy" "network_admin_policy" {
+  name = "network_admin_policy"
+  role = aws_iam_role.network_admin.id
 
-
-resource "aws_instance" "my_vm" {
-  ami           = "ami-053b0d53c279acc90" //Ubuntu AMI
-  instance_type = "t2.micro"
-  user_data     = file("gui/init.sh")
-  tags = {
-    Name = "My E3333",
-  }
-}
-
-resource "aws_security_group" "my_vm" {
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-resource "aws_iam_user_policy" "test_policy" {
- name = "name"
- user = "terra-user"
-
- policy = <<EOF
+  policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [{
    "Effect": "Allow",
    "Action": [
-   "ec2:DescribeInstances"
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:CreateNetworkInterfacePermission",
+          "ec2:CreateTags",
+          "ec2:ModifyNetworkInterfaceAttribute",
+          "ec2:DescribeNetworkInterfacePermissions"
    ],
    "Resource": "*"
  }]
 }
  EOF
 }
+resource "aws_iam_role" "network_admin" {
+  name = "network_admin"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+          AWS     = "arn:aws:iam::036683246906:user/terra-admin-user"
+        }
+      },
+    ]
+  })
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+resource "aws_iam_role_policy" "compute_admin_policy" {
+  name = "compute_admin_policy"
+  role = aws_iam_role.compute_admin.id
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [{
+   "Effect": "Allow",
+   "Action": [
+                "ec2:CreateTags",
+                "ec2:RunInstances"
+   ],
+   "Resource": "*"
+ }]
+}
+ EOF
+}
+resource "aws_iam_role" "compute_admin" {
+  name = "compute_admin"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+          AWS     = "arn:aws:iam::036683246906:user/terra-admin-user"
+        }
+      },
+    ]
+  })
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy" "data_engineer_policy" {
+  name = "data_engineer_policy"
+  role = aws_iam_role.data_engineer.id
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [{
+   "Effect": "Allow",
+   "Action": [
+            "s3:PutAnalyticsConfiguration",
+                "s3:PutAccessPointConfigurationForObjectLambda",
+                "s3:GetObjectVersionTagging",
+                "s3:PutStorageLensConfiguration",
+                "s3:DeleteAccessPoint",
+                "s3:CreateBucket",
+                "s3:DeleteAccessPointForObjectLambda",
+                "s3:GetStorageLensConfigurationTagging",
+                "s3:ReplicateObject",
+                "s3:GetObjectAcl",
+                "s3:GetBucketObjectLockConfiguration",
+                "s3:DeleteBucketWebsite",
+                "s3:GetIntelligentTieringConfiguration",
+                "s3:PutLifecycleConfiguration",
+                "s3:GetObjectVersionAcl",
+                "s3:DeleteObject",
+                "s3:CreateMultiRegionAccessPoint",
+                "s3:GetBucketPolicyStatus",
+                "s3:GetObjectRetention",
+                "s3:GetBucketWebsite",
+                "s3:GetJobTagging",
+                "s3:GetMultiRegionAccessPoint",
+                "s3:PutReplicationConfiguration",
+                "s3:GetObjectAttributes",
+                "s3:PutObjectLegalHold",
+                "s3:InitiateReplication",
+                "s3:GetObjectLegalHold",
+                "s3:GetBucketNotification",
+                "s3:PutBucketCORS",
+                "s3:DescribeMultiRegionAccessPointOperation",
+                "s3:GetReplicationConfiguration",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:PutBucketNotification",
+                "s3:DescribeJob",
+                "s3:PutBucketLogging",
+                "s3:GetAnalyticsConfiguration",
+                "s3:PutBucketObjectLockConfiguration",
+                "s3:GetObjectVersionForReplication",
+                "s3:CreateJob",
+                "s3:GetAccessPointForObjectLambda",
+                "s3:GetStorageLensDashboard",
+                "s3:CreateAccessPoint",
+                "s3:GetLifecycleConfiguration",
+                "s3:GetAccessPoint",
+                "s3:GetInventoryConfiguration",
+                "s3:GetBucketTagging",
+                "s3:PutAccelerateConfiguration",
+                "s3:SubmitMultiRegionAccessPointRoutes",
+                "s3:GetAccessPointPolicyForObjectLambda",
+                "s3:DeleteObjectVersion",
+                "s3:GetBucketLogging",
+                "s3:RestoreObject",
+                "s3:GetAccelerateConfiguration",
+                "s3:GetObjectVersionAttributes",
+                "s3:GetBucketPolicy",
+                "s3:PutEncryptionConfiguration",
+                "s3:GetEncryptionConfiguration",
+                "s3:GetObjectVersionTorrent",
+                "s3:AbortMultipartUpload",
+                "s3:GetBucketRequestPayment",
+                "s3:GetAccessPointPolicyStatus",
+                "s3:UpdateJobPriority",
+                "s3:GetObjectTagging",
+                "s3:GetMetricsConfiguration",
+                "s3:GetBucketOwnershipControls",
+                "s3:DeleteBucket",
+                "s3:PutBucketVersioning",
+                "s3:GetBucketPublicAccessBlock",
+                "s3:GetMultiRegionAccessPointPolicyStatus",
+                "s3:PutIntelligentTieringConfiguration",
+                "s3:GetMultiRegionAccessPointPolicy",
+                "s3:GetAccessPointPolicyStatusForObjectLambda",
+                "s3:PutMetricsConfiguration",
+                "s3:PutBucketOwnershipControls",
+                "s3:DeleteMultiRegionAccessPoint",
+                "s3:UpdateJobStatus",
+                "s3:GetBucketVersioning",
+                "s3:GetBucketAcl",
+                "s3:GetAccessPointConfigurationForObjectLambda",
+                "s3:PutInventoryConfiguration",
+                "s3:GetObjectTorrent",
+                "s3:GetMultiRegionAccessPointRoutes",
+                "s3:GetStorageLensConfiguration",
+                "s3:DeleteStorageLensConfiguration",
+                "s3:GetAccountPublicAccessBlock",
+                "s3:PutBucketWebsite",
+                "s3:PutBucketRequestPayment",
+                "s3:PutObjectRetention",
+                "s3:CreateAccessPointForObjectLambda",
+                "s3:GetBucketCORS",
+                "s3:GetBucketLocation",
+                "s3:GetAccessPointPolicy",
+                "s3:ReplicateDelete",
+                "s3:GetObjectVersion",
+                	"s3:ListBucket",
+                    "s3:ListAllMyBuckets"
+                
+   ],
+   "Resource": "*"
+ }]
+}
+ EOF
+}
+resource "aws_iam_role" "data_engineer" {
+  name = "data_engineer"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+          AWS     = "arn:aws:iam::036683246906:user/terra-admin-user"
+        }
+      },
+    ]
+  })
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+
+
+
+
